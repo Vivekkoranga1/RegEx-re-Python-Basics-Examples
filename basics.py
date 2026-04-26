1615b58aeeea5dc5fd3e746e26abaca47e6c6084
vivek <vivekkoranga01@gmail.com>
2026-04-26 11:38:46 +0530

    basics.py completed

--- basics.py
+++ basics.py
@@ -112,3 +112,133 @@ print(using_dot)
 # *	        |    \*
 # ?	        |    \?
 
+##way to find real dot "." using backslash "\"
+## "." "+" "*" "?" are special character so when we use \ like "\." we escape the special character and also use for making special sequences
+find_dot = re.findall(pattern="\.",string=text)
+print(find_dot)
+#Ouptput:['.']
+
+
+##THE IDEA OF RAW STRING 
+##we are working with special charcters and meta data but we can face some problem like 
+##"\n" this give us new line and "\b" this is used for back slash 
+##but when we try to use them in RegularExpression python process them before they reach to "RegularExpressions"
+##so we use raw string FORMAT = r"" or R""
+##We use raw strign because we dont want future problem 
+##e.g 
+##with raw string 
+print(r"Hello\nWorld")
+##Output:Hello\nWorld
+##without raw string 
+print("Hello\nWorld")
+##Output:Hello
+##World
+
+
+##it is part of meta character
+# # \ (Backslash): Escapes a character, treating it as a literal rather than a special character.
+##for escaping "\n" new line we can also use ("\\n" "\\" become "\") 
+print("Hello\\nWorld")
+##OutPut:Hello\nWorld
+print("Hello\\World")
+##Output:Hello\World
+print("Hello\nWorld")
+##OutPut:Hello
+##World
+
+
+
+##Use  of ^ (Caret)
+##Matches the start of a string or line. work like re.match we have already read above
+star_with_Hello_using_caret = re.findall(r"^Hello",string=text)
+print(star_with_Hello_using_caret)
+##Output:['Hello']
+
+
+
+# # $ (Dollar Sign): Matches the end of a string or line.
+##eg. checking 2026 coming at the end of text or not 
+check_2026_at_end_using_dollar = re.findall(r"2026$",text)
+print(check_2026_at_end_using_dollar)
+##output:['2026']
+
+
+# # * (Asterisk): Matches zero or more occurrences of the preceding character.
+new_text_for_asterisk = "bbbbbbb"
+##many occurence
+print(re.findall(r"b*",new_text_for_asterisk))
+##Output:['bbbbbbb', ''] it also give '' because it zero or more matches no match also give ''
+##zero match 
+print(re.findall("a*","bbbbbbbbbbbbbbbbb"))
+##output:['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''] ## every time 0 match so it give ""...
+##WE WILL DO ADVANCE THINGS IN PROJECT HERE WE ARE LEARNING BASICS
+
+
+
+
+# # + (Plus): Matches one or more occurrences.
+##if zero_occurence
+print(re.findall(r"b+",string="aaaaaaaa"))
+# Output:[]
+##if one occurence
+print(re.findall(r"b+",string="aaaaaaab"))
+# Output:['b']
+##if more occuernce
+print(re.findall(r"b+",string="bbbbbbb"))
+#Output:['bbbbbbb']
+
+
+
+# # ? (Question Mark): Matches zero or one occurrence.
+##if zero_occurence
+print(re.findall(r"b?",string="aa"))
+##Output:['', '', ''] it try to check b in aa then it will move forward and check but there is nothing so it also match to 0 times that's why '' coming three times
+##if one occurence
+print(re.findall(r"b?",string="b"))
+##Output:['b', '']
+##if one occurence
+print(re.findall(r"b?",string="bbbbb"))
+##Output:['b', 'b', 'b', 'b', 'b', '']
+
+
+
+
+# # [] (Square Brackets): Defines a character class/set (e.g., [a-z]).
+print(re.findall(r"[a-z]",text,flags=re.IGNORECASE))
+##OutPut:['H', 'e', 'l', 'l', 'o', 'I', 'a', 'm', 'v', 'i', 'v', 'e', 'k', 'I', 'a', 'm', 'f', 'r', 'o', 'm', 'I', 'n', 'd', 'i', 'a', 'a', 'n', 'd', 'i', 'a', 'm', 'p', 'a', 'r', 't', 'o', 'f', 'S', 't', 'a', 'n', 'f', 'o', 'r', 'd', 'C', 'o', 'd', 'e', 'i', 'n', 'p', 'l', 'a', 'c', 'e']
+##we are getting everything except 2026
+##better approach
+print(re.findall(r"[a-z]+",text,flags=re.IGNORECASE)) ##matching one or more 
+##Output ['Hello', 'I', 'am', 'vivek', 'I', 'am', 'from', 'India', 'and', 'i', 'am', 'part', 'of', 'Stanford', 'Code', 'in', 'place']
+
+
+
+
+
+##| (Pipe): Acts as an OR operator (e.g., A|B).
+print(re.findall(r"vivek|code",text,flags=re.IGNORECASE))
+##Output['vivek', 'Code']
+
+
+# # () (Parentheses): Groups characters for capture.
+##with parenthesis 
+print(re.findall(r"(ab)+","ab abb abbb abab ababab")) ## here one or more number of ab 
+##output:['ab', 'ab', 'ab', 'ab', 'ab']
+##without parenthesis 
+print(re.findall(r"ab+","ab abb abbb abab ababab")) ## wher ab or abb or abbb like that 
+##output:['ab', 'abb', 'abbb', 'ab', 'ab', 'ab', 'ab', 'ab']
+
+
+
+
+##{} (Curly Braces): Specifies a quantifier (number of occurrences). 
+##exact match
+print(re.findall(r"a{2}", "a aa aaa"))
+##Output:['aa', 'aa']
+
+
+
+##See i want to tell you that we can find words or text using normal pattern but we use "meta characters" for validation"
+##like from a paragrah finding all the valid emails and valid mobile numbers
+## like college use for while accessing their portal it should exactly match @particularcollege.com
+##we will do many examples related to validation like email and phone number validation in this repository. 
